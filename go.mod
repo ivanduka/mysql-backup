93b936e811b7a6220c9adac6c19b03f083bf2eaa
Ivan Duka <ivan.duka.pm@gmail.com>
2020-04-22 14:29:23 -0600

    db connection and list of databases

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module main
 
 go 1.13
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+import "github.com/joho/godotenv"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("cannot load environmental variables")
+	}
+
+	connStr := fmt.Sprintf("%v:%v@tcp(%v:%v)/",
+		os.Getenv("DB_NAME"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	fmt.Println(connStr)
+
+	db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	result, err := db.Query("SHOW DATABASES;")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var databases []string
+	for result.Next() {
+		var database string
+		err = result.Scan(&database)
+		if err != nil {
+			log.Fatal(err)
+		}
+		databases = append(databases, database)
+	}
+	fmt.Println(databases)
 }
